02a7d0d312515789d318525562413e7b417e1043
poyrazovic <sertacpoyrazz@gmail.com>
2018-10-15 19:02:14 +0300

    project init

---
+++ pages/404.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  section.not-found
+    h1 404!
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+  head: () => ({
+    title: '404'
+  })
+}
+</script>
+
+<style lang="sass">
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  section.login
+    h1 Login
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  head: () => ({
+    title: 'Login'
+  })
+}
+</script>
+
+<style lang="sass">
+</style>
